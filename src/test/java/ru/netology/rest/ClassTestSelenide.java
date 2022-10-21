60618e0bca098c0f8519fe8ad4355eb92216540a
Bln2108 <bln2108@yandex.ru>
2022-10-21 18:31:22 +0300

    первое изменение

---
+++ src/test/java/ru/netology/rest/ClassTestSelenide.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ClassTestSelenide {
+
+    @Test
+    void shouldRegisterByAccountNumberVisibilityChange() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Новосибирск");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(" ");
+        $("[data-test-id='date'] input").setValue("25.10.2022");
+        $("[data-test-id='name'] input").setValue("Дмитрий Петров");
+        $("[data-test-id='phone'] input").setValue("+79200000000");
+        $("[data-test-id='agreement']").click();
+        $(byText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
